55e72fba6cc2c9980354fe219e8fa4da7fbcc865
Cade Schreger <cschreger@gmail.com>
2020-08-17 21:58:41 -0400

    Github fix?

---
+++ src/scripts/marvel_chars.js
@@ -0,0 +1,20 @@
+// function getSpiderman() {
+//     fetch('')
+// }
+
+// let request = new XMLHttpRequest();
+// request.open("GET", "")
+
+// let params: {
+//   "apikey": "your api key",
+//   "ts": "a timestamp",
+//   "hash": "your hash"
+// }
+// Headers: {
+//   Accept: */*
+// }
+
+
+// fetch('http://example.com/movies.json')
+//   .then(response => response.json())
+//   .then(data => console.log(data));
