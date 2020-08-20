3d4a410579f4267349ce496753799e74cc1150fb
Cade Schreger <cschreger@gmail.com>
2020-08-20 18:16:20 -0400

    Multi-line graph with mouse-tracking bug

--- src/scripts/market_share.js
+++ src/scripts/market_share.js
@@ -2,7 +2,7 @@ export const marketShare = () => {
 
 d3.csv('../data/market_share.csv').then(d => graph(d))
 
-debugger
+
 function graph(data) {
 
     const keys = data.columns.slice(1);
@@ -10,9 +10,9 @@ function graph(data) {
     const width = 900 - margin.left - margin.right
     const height = 600 - margin.top - margin.bottom
 
-    // data.forEach(function(d) {
-    //     return d
-    // })
+    data.forEach(function(d) {
+        return d
+    })
     
     const xScale = d3.scaleTime().range([0,width])
     const yScale = d3.scaleLinear().range([height, 0])
@@ -24,13 +24,13 @@ function graph(data) {
 
     
     const line = d3.line()
-    .curve(d3.curveCardinal)
     .x(function(d) {
         return xScale(d.year)
     })
     .y(function(d) {
         return yScale(d.share)
     })
+    .curve(d3.curveCardinal)
     
     const svg = d3.select("body")
         .append("svg")
@@ -65,11 +65,12 @@ function graph(data) {
                 return v.share
             })
         }),
-        d3.max(companies, function(c) {
-            return d3.max(c.values, function(v) {
-                return v.share
-            })
-        })
+        // d3.max(companies, function(c) {
+        //     return d3.max(c.values, function(v) {
+        //         return v.share
+        //     })
+        // })
+        50
     ])
 
     const legend = svg.selectAll('g')
@@ -119,7 +120,6 @@ function graph(data) {
         .append("g")
         .attr("class", "company")
 
-    debugger
     company.append("path")
         .attr("class", "line")
         .attr("d", function(d) {
@@ -131,21 +131,110 @@ function graph(data) {
 
     debugger
     company.append("text")
-        .datum(function(d) {
-            return {
-                name: d.name,
-                value: d.values[d.values.length - 1]
-            }
+    .datum(function(d) {
+        return {
+            name: d.name,
+            value: d.values[d.values.length - 1]
+        }
+    })
+    .attr("transform", function(d) {
+        return "translate(" + xScale(d.value.year) + "," + yScale(d.value.share) + ")"
+    })
+    .attr("x", 3)
+    .attr("dy", ".35em")
+    .text(function(d) {
+        return d.name
+    })
+    
+    // Credit to larsenmtl(bl.ocks.org/larsenmtl)
+    const mouseG = svg.append("g")
+        .attr("class", "mouse-over-effects");
+
+    mouseG.append("path") // this is the black vertical line to follow mouse
+        .attr("class", "mouse-line")
+        .style("stroke", "black")
+        .style("stroke-width", "1px")
+        .style("opacity", "0");
+
+    const lines = document.getElementsByClassName('line');
+
+    const mousePerLine = mouseG.selectAll('.mouse-per-line')
+        .data(companies)
+        .enter()
+        .append("g")
+        .attr("class", "mouse-per-line");
+
+    mousePerLine.append("circle")
+        .attr("r", 7)
+        .style("stroke", function (d) {
+            return color(d.name);
         })
-        .attr("transform", function(d) {
-            return "translate(" + xScale(d.year) + "," + yScale(d.value.share) + ")"
+        .style("fill", "none")
+        .style("stroke-width", "1px")
+        .style("opacity", "0");
+
+    mousePerLine.append("text")
+        .attr("transform", "translate(10,3)");
+
+    mouseG.append('svg:rect') // append a rect to catch mouse movements on canvas
+        .attr('width', width) // can't catch mouse events on a g element
+        .attr('height', height)
+        .attr('fill', 'none')
+        .attr('pointer-events', 'all')
+        .on('mouseout', function () { // on mouse out hide line, circles and text
+            d3.select(".mouse-line")
+                .style("opacity", "0");
+            d3.selectAll(".mouse-per-line circle")
+                .style("opacity", "0");
+            d3.selectAll(".mouse-per-line text")
+                .style("opacity", "0");
         })
-        .attr("x", 3)
-        .attr("dy", ".35em")
-        .text(function(d) {
-            return d.name
+        .on('mouseover', function () { // on mouse in show line, circles and text
+            d3.select(".mouse-line")
+                .style("opacity", "1");
+            d3.selectAll(".mouse-per-line circle")
+                .style("opacity", "1");
+            d3.selectAll(".mouse-per-line text")
+                .style("opacity", "1");
         })
+        .on('mousemove', function () { // mouse moving over canvas
+            const mouse = d3.mouse(this);
+            d3.select(".mouse-line")
+                .attr("d", function () {
+                    let d = "M" + mouse[0] + "," + height;
+                    d += " " + mouse[0] + "," + 0;
+                    return d;
+                });
+
+            d3.selectAll(".mouse-per-line")
+                .attr("transform", function (d, i) {
+                    const xYear = xScale.invert(mouse[0]),
+                        bisect = d3.bisector(function (d) { return d.year; }).right;
+                    const idx = bisect(d.values, xYear);
+
+                    let beginning = 0,
+                        end = lines[i].getTotalLength(),
+                        target = null;
+
+                    while (true) {
+                        const target = Math.floor((beginning + end) / 2);
+                        let pos = lines[i].getPointAtLength(target);
+                        if ((target === end || target === beginning) && pos.x !== mouse[0]) {
+                            break;
+                        }
+                        if (pos.x > mouse[0]) end = target;
+                        else if (pos.x < mouse[0]) beginning = target;
+                        else break; //position found
+                    }
+
+                    d3.select(this).select('text')
+                        .text(yScale.invert(pos.y).toFixed(2));
+
+                    return "translate(" + mouse[0] + "," + pos.y + ")";
+                });
+        });
 
 }
 
 }
+
